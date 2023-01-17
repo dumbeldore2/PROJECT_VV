9057d1525a759fe26d09e04577328ccb85f6bee7
PC001 laptop <yago.engels@gmail.com>
2023-01-17 19:21:56 +0100

    aanmaken MainActivity2_login.java en refactoring van MainActivity1_startscreen.java JAVA EN RES file

--- app/src/main/java/com/example/project_vv/MainActivity1_startscreen.java
+++ app/src/main/java/com/example/project_vv/MainActivity1_startscreen.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_activity1_startscreen);
 
         //conecten textviews
         textView1 = findViewById(R.id.title);
